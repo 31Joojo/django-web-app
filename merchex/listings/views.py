d65f7c6278fa19f8dd2cec7efbfdd11fc6dfaea2
Joris LARMAILLARD <jorislarmaillard@gmail.com>
2024-08-07 16:52:40 -0400

    Ajout d'un gabarit pour l'affichage des éléments de la base de données

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -6,36 +6,8 @@ from listings.models import Band, Announcement
 def hello(request):
     bands = Band.objects.all()
     announcements = Announcement.objects.all()
-    return HttpResponse(f"""
-        <h1>Hello Django!</h1>
-        <p>Mes groupes préférés sont :</p>
-        <ul>
-            <li>
-                <section>
-                    {bands[0].name} <br>
-                    {announcements[0].title}
-                </section>
-            </li>
-            <li>
-                <section>
-                    {bands[1].name} <br>
-                    {announcements[1].title}
-                </section>
-            </li>
-            <li>
-                <section>
-                    {bands[2].name} <br>
-                    {announcements[2].title}
-                </section>
-            </li>
-            <li>
-                <section>
-                    {bands[3].name} <br>
-                    {announcements[3].title}
-                </section>
-            </li>
-        </ul>
-        """)
+    return render(request, 'listings/hello.html',
+                  {'bands': bands})
 
 def about(request):
     return HttpResponse('<h1>About us!</h1> <p>We love merch! </p>')
@@ -43,5 +15,5 @@ def about(request):
 def contacts(request):
     return HttpResponse('<h1>Contact us</h1>')
 
-def ads(request) :
+def ads(request):
     return HttpResponse('<h1>Ads available</h1>')
