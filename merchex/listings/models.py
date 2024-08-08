5fb7767c53ab6f18a6846481987e3310aeb54c44
Joris LARMAILLARD <jorislarmaillard@gmail.com>
2024-08-08 15:22:53 -0400

    Ajout de champs dans le modèle Band et ajout d'une sous-classe pour le choix des différents genres

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,6 +1,20 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Band(models.Model):
+    ## Sous-classe pour répertorier les différents genres possibles
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+
     name = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField(choices=Genre.choices, max_length=50)
+    biography = models.fields.CharField(max_length=1000)
+    year_formed = models.fields.IntegerField(
+        validators=[MinValueValidator(1900), MaxValueValidator(2024)]
+    )
+    active = models.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
 class Announcement(models.Model):
     title = models.fields.CharField(max_length=100)
