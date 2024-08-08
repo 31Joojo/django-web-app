73fc90df8583f7d2956b19abe0adcc405007ceae
Joris LARMAILLARD <jorislarmaillard@gmail.com>
2024-08-08 15:46:31 -0400

    Ajout de nouveaux champs dans le modèle "Announcement"

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -7,9 +7,11 @@ class Band(models.Model):
         HIP_HOP = 'HH'
         SYNTH_POP = 'SP'
         ALTERNATIVE_ROCK = 'AR'
+        K_POP = 'KP'
+        ZOUK = 'ZK'
 
     name = models.fields.CharField(max_length=100)
-    genre = models.fields.CharField(choices=Genre.choices, max_length=50)
+    genre = models.fields.CharField(choices=Genre.choices, max_length=5)
     biography = models.fields.CharField(max_length=1000)
     year_formed = models.fields.IntegerField(
         validators=[MinValueValidator(1900), MaxValueValidator(2024)]
@@ -17,4 +19,17 @@ class Band(models.Model):
     active = models.BooleanField(default=True)
     official_homepage = models.fields.URLField(null=True, blank=True)
 class Announcement(models.Model):
+    ## Sous-classe pour répertorier les différents types
+    class Type(models.TextChoices):
+        RECORDS = 'RE'
+        CLOTHING = 'CL'
+        POSTERS = 'PO'
+        MISCELLANEOUS = 'MI'
+
     title = models.fields.CharField(max_length=100)
+    description = models.fields.CharField(max_length=300)
+    sold = models.fields.BooleanField(default=False)
+    year = models.fields.IntegerField(
+        validators=[MinValueValidator(1900), MaxValueValidator(2024)]
+    )
+    type = models.fields.CharField(choices=Type.choices, max_length=5)
