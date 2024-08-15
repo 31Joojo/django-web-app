c10a200c0206f4d6fd11f4181d6d67170c850979
Joris LARMAILLARD <jorislarmaillard@gmail.com>
2024-08-15 17:26:27 -0400

    Ajout d'un formulaire pour la création d'une annonce

--- merchex/listings/forms.py
+++ merchex/listings/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
-from listings.models import Band
+from listings.models import Band, Listing
+
 
 class ContactForm(forms.Form):
     name = forms.CharField(required=False, max_length=100)
@@ -11,4 +12,10 @@ class BandForm(forms.ModelForm):
     ## Classe imbriquée qui définit quel modèle et quels champs du modèle à utiliser
     class Meta:
         model = Band
-        fields = '__all__'
+        ## fields = '__all__'
+        exclude = ('active', 'official_homepage')
+
+class ListingForm(forms.ModelForm):
+    class Meta:
+        model = Listing
+        exclude = ('sold',)
