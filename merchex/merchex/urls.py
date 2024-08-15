a97c290f5ae751e88e1db422c2d21a2af491d2e0
Joris LARMAILLARD <jorislarmaillard@gmail.com>
2024-08-15 17:30:06 -0400

    Ajout des modèles d'URL pour la création et la modification d'une annonce

--- merchex/merchex/urls.py
+++ merchex/merchex/urls.py
@@ -23,10 +23,13 @@ urlpatterns = [
     path("bands/", views.band_list, name="band-list"),
     path("bands/<int:band_id>/", views.band_detail, name="band-detail"),
     path("bands/add/", views.band_create, name="band-create"),
+    path("bands/<int:band_id>/udpate", views.band_update, name="band-update"),
     path("about-us/", views.about, name="about-us"),
     path("contact-us/", views.contact, name="contact-us"),
     path("ads/", views.ads, name="ads-list"),
     path("ads/<int:ad_id>/", views.ad_detail, name="ad-detail"),
+    path("ads/add", views.ad_create, name="ad-create"),
+    path("ads/<int:ad_id>/update/", views.ad_update, name="ad-update"),
     path("confirmation/", views.confirmation, name="email-sent"),
     path("", views.band_list, name="home")
 ]
