835115c1f8126ebf112dc859f81bdb0fa8ea0daf
Joris LARMAILLARD <jorislarmaillard@gmail.com>
2024-08-15 15:31:30 -0400

    Ajout d'un formulaire pour l'ajout d'un groupe

--- merchex/listings/forms.py
+++ merchex/listings/forms.py
@@ -1,6 +1,14 @@
 from django import forms
 
+from listings.models import Band
+
 class ContactForm(forms.Form):
     name = forms.CharField(required=False, max_length=100)
     email = forms.EmailField()
     message = forms.CharField(max_length=1000)
+
+class BandForm(forms.ModelForm):
+    ## Classe imbriquée qui définit quel modèle et quels champs du modèle à utiliser
+    class Meta:
+        model = Band
+        fields = '__all__'
