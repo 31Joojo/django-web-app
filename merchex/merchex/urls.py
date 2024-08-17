1dc469379399181b7e2acdb61d55157105356692
Joris LARMAILLARD <jorislarmaillard@gmail.com>
2024-08-17 19:50:27 -0400

    Configuration des fichiers médias et des urls

--- merchex/merchex/urls.py
+++ merchex/merchex/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from listings import views
 
 urlpatterns = [
@@ -35,3 +37,6 @@ urlpatterns = [
     path("confirmation/", views.confirmation, name="email-sent"),
     path("", views.band_list, name="home")
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
